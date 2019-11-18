7c23264bc851125d57beb7a1ef33ccdb75678a87
masa23vn <masa23vn@gmail.com>
2019-11-19 02:05:02 +0700

    finish product list

---
+++ Product list.js
@@ -0,0 +1,11 @@
+function checkOffset() {
+    if($('.sidebar-container').offset().top + $('.sidebar-container').height() 
+                                           >= $('.footer').offset().top - 10)
+        $('.sidebar-container').css('position', 'absolute');
+    if($(document).scrollTop() + window.innerHeight < $('.footer').offset().top)
+        $('.sidebar-container').css('position', 'fixed'); // restore when you scroll up
+}
+$(document).scroll(function() {
+    checkOffset();
+});
+
