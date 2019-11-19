e1d1954e56dafae74da740e62c372edf40ecae91
DengJunMing <jumming456@gmail.com>
2019-11-19 23:16:29 +0700

    re-clone from master

--- javascript.js
+++
@@ -1,8 +0,0 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-var el = document.getElementById("category");
-el.onchange = function(){
-	var category = $('#category :selected').attr('class');
-  $('#subcategory optgroup').hide();
-  $('#subcategory').val("");
-  $('#subcategory .'+category).show();}
-  
