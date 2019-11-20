788e4a7ea99cf4aa625e577e78deefc660d7f510
masa23vn <masa23vn@gmail.com>
2019-11-20 23:16:13 +0700

    merge and add link

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -17,3 +17,14 @@ $(()=>{
     $('#logreg-forms #btn-signup').click(toggleSignUp);
     $('#logreg-forms #cancel_signup').click(toggleSignUp);
 })
+
+// Open sign-up from link
+if(window.location.hash) {
+    var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+    if(hash == "sign-up") {
+        $('#logreg-forms .form-signin').toggle(); // display:block or none
+        $('#logreg-forms .form-signup').toggle(); // display:block or none
+    }
+} else {
+    // No hash found
+}
