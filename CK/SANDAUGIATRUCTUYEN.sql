2b18fe1c85fd5e5e45da67ed6b2ed4d90db92190
masa23vn <masa23vn@gmail.com>
2019-12-12 01:12:47 +0700

    Update SANDAUGIATRUCTUYEN.sql

--- CK/SANDAUGIATRUCTUYEN.sql
+++ CK/SANDAUGIATRUCTUYEN.sql
@@ -21,7 +21,7 @@ CREATE TABLE SANPHAM(
 	ID INT NOT NULL AUTO_INCREMENT,
 	TenSanPham VARCHAR(1000) NOT NULL,
 	Gia FLOAT NOT NULL,
-    GiaMuaNgay FLOAT,
+	GiaMuaNgay FLOAT,
 	ThoiGianConLai TIME,
 	NgayDang DATETIME NOT NULL,
     MoTaNgan VARCHAR(100),
@@ -34,10 +34,17 @@ CREATE TABLE SANPHAM(
 	PRIMARY KEY(ID)
 );
 
-CREATE TABLE LOAI(
+CREATE TABLE LOAICAP1(
 	ID INT NOT NULL AUTO_INCREMENT,
 	TenLoai VARCHAR(100) NOT NULL,
 	PRIMARY KEY(ID)
+);
+
+CREATE TABLE LOAICAP2(
+	ID INT NOT NULL AUTO_INCREMENT,
+	IDLoaiCap1 INT NOT NULL,
+	TenLoai VARCHAR(100) NOT NULL,
+	PRIMARY KEY(ID, IDLoaiCap1)
 );
 
 CREATE TABLE HINHANH(
@@ -70,6 +77,12 @@ CREATE TABLE SANPHAMYEUTHICH(
 	PRIMARY KEY(IDSanPham, IDNguoiDung)
 );
 
+CREATE TABLE DANHSACHCAM(
+	IDNguoiDung INT NOT NULL,
+	IDSanPham INT NOT NULL,
+	PRIMARY KEY(IDNguoiDung, IDSanPham)
+);
+
 CREATE TABLE THAMSO(
 	ID INT NOT NULL AUTO_INCREMENT,
 	ChiPhiDangSanPham FLOAT,
@@ -77,7 +90,8 @@ CREATE TABLE THAMSO(
 );
 
 ALTER TABLE SANPHAM ADD CONSTRAINT FK_SANPHAM_NGUOIDUNGBAN FOREIGN KEY(IDNguoiBan) REFERENCES NGUOIDUNG(ID);
-ALTER TABLE SANPHAM ADD CONSTRAINT FK_SANPHAM_LOAI FOREIGN KEY(IDLoai) REFERENCES LOAI(ID);
+ALTER TABLE SANPHAM ADD CONSTRAINT FK_SANPHAM_LOAI FOREIGN KEY(IDLoai) REFERENCES LOAICAP2(ID);
+ALTER TABLE LOAICAP2 ADD CONSTRAINT FK_LOAICAP2_LOAICAP1 FOREIGN KEY(IDLoaiCap1) REFERENCES LOAICAP1(ID);
 ALTER TABLE SANPHAM ADD CONSTRAINT FK_SANPHAM_NGUOIDUNGTHANG FOREIGN KEY(IDNguoiThangDauGia) REFERENCES NGUOIDUNG(ID);
 ALTER TABLE HINHANH ADD CONSTRAINT FK_HINHANH_SANPHAM FOREIGN KEY(IDSanPham) REFERENCES SANPHAM(ID);
 ALTER TABLE CHITIETDAUGIA ADD CONSTRAINT FK_CHITIETDAUGIA_NGUOIDUNGDAUGIA FOREIGN KEY(IDNguoiDauGia) REFERENCES NGUOIDUNG(ID);
@@ -85,63 +99,77 @@ ALTER TABLE CHITIETDAUGIA ADD CONSTRAINT FK_CHITIETDAUGIA_SANPHAM FOREIGN KEY(ID
 ALTER TABLE CHITIETDANHGIA ADD CONSTRAINT FK_CHITIETDANHGIA_NGUOIDUNGDUOCDANHGIA FOREIGN KEY(IDNguoiDuocDanhGia) REFERENCES NGUOIDUNG(ID);
 ALTER TABLE CHITIETDANHGIA ADD CONSTRAINT FK_CHITIETDANHGIA_NGUOIDUNGDANHGIA FOREIGN KEY(IDNguoiDanhGia) REFERENCES NGUOIDUNG(ID);
 ALTER TABLE SANPHAMYEUTHICH ADD CONSTRAINT FK_SANPHAMYEUTHICH_NGUOIDUNG FOREIGN KEY(IDNguoiDung) REFERENCES NGUOIDUNG(ID);
-ALTER TABLE SANPHAMYEUTHICH ADD CONSTRAINT FK_SANPHAMYEUTHICH_SANPchitietdanhgiaHAM FOREIGN KEY(IDSanPham) REFERENCES SANPHAM(ID);
+ALTER TABLE SANPHAMYEUTHICH ADD CONSTRAINT FK_SANPHAMYEUTHICH_SANPHAM FOREIGN KEY(IDSanPham) REFERENCES SANPHAM(ID);
+ALTER TABLE DANHSACHCAM ADD CONSTRAINT FK_DANHSACHCAM_SANPHAM FOREIGN KEY(IDSanPham) REFERENCES SANPHAM(ID);
+ALTER TABLE DANHSACHCAM ADD CONSTRAINT FK_DANHSACHCAM_NGUOIDUNG FOREIGN KEY(IDNguoiDung) REFERENCES NGUOIDUNG(ID);
 
-INSERT INTO LOAI(ID, TenLoai) VALUES
+INSERT INTO LOAICAP1(ID, TenLoai) VALUES
 (null, 'Điện tử'),
-(null, 'Quần áo'),
-(null, 'Thực phẩm');
+(null, 'Thời trang'),
+(null, 'Phương tiện'),
+(null, 'Đồ chơi giải trí');
+
+INSERT INTO LOAICAP2(ID, IDLoaiCap1, TenLoai) VALUES
+(null, 1, 'Điện thoại thông minh'),
+(null, 1, 'Laptop'),
+(null, 1, 'Linh kiện máy tính'),
+(null, 2, 'Áo khoác'),
+(null, 2, 'Quần tây'),
+(null, 2, 'Bikini'),
+(null, 3, 'Xe máy'),
+(null, 3, 'Ôtô'),
+(null, 4, 'Máy chơi game'),
+(null, 4, 'Đồ chơi');
 
 -- Loai: 1: Bidder, 2: Seller, 3: Admin
 -- Folder avatar chứa avatar của user. Tên hình: {{ID}}.jpg
 INSERT INTO NGUOIDUNG(ID, TenTaiKhoan, MatKhau, HoTen, NgaySinh, Email, DiaChi, DienThoai, Loai, AvatarURL, TongDiemDanhGia) VALUES
-(null, 'admin', '123', 'Ad Văn Min', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 3, 'assets/images/avatar/1.jpg', 10),
-(null, 'seller1', '123', 'Sel Văn Ler', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/2.jpg', 9),
-(null, 'bidder1', '123', 'Bid Văn Der', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/3.jpg', 9),
-(null, 'seller2', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/4.jpg', 9),
-(null, 'bidder2', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/5.jpg', 9),
-(null, 'seller3', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/6.jpg', 9),
-(null, 'bidder3', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/7.jpg', 9),
-(null, 'seller4', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/8.jpg', 9),
-(null, 'bidder4', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/9.jpg', 6),
-(null, 'seller5', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/10.jpg', 9),
-(null, 'bidder5', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/11.jpg', 7),
-(null, 'seller6', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/12.jpg', 9),
-(null, 'bidder6', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/13.jpg', 8),
-(null, 'seller7', '123', 'Nguyễn Văn A', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/14.jpg', 9);
+(null, 'admin', '123', 'Tân Sơn Nhất', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 3, 'assets/images/avatar/1.jpg', 10),
+(null, 'seller1', '123', 'Triệu Thị Hai', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/2.jpg', 9),
+(null, 'bidder1', '123', 'Nguyễn Văn Ba', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/3.jpg', 9),
+(null, 'seller2', '123', 'Trịnh Minh Tư', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/4.jpg', 9),
+(null, 'bidder2', '123', 'Phạm Hoài Năm', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/5.jpg', 9),
+(null, 'seller3', '123', 'Võ Thị Sáu', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/6.jpg', 9),
+(null, 'bidder3', '123', 'Lâm Văn Bảy', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/7.jpg', 9),
+(null, 'seller4', '123', 'Lê Văn Tám', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/8.jpg', 9),
+(null, 'bidder4', '123', 'Lại Hoàng Chín', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/9.jpg', 6),
+(null, 'seller5', '123', 'Trần Văn Mười', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/10.jpg', 9),
+(null, 'bidder5', '123', 'Mai Hoàng Linh Phi', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/11.jpg', 7),
+(null, 'seller6', '123', 'Trần Nguyễn Thanh Trúc', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/12.jpg', 9),
+(null, 'bidder6', '123', 'Quách Thị Minh Trang', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 1, 'assets/images/avatar/13.jpg', 8),
+(null, 'seller7', '123', 'Lâm Lê Bảo Nam', '1999-01-01', 'someone@example.com', '227 Nguyễn Văn Cừ, Q5, TPHCM', '0987654321', 2, 'assets/images/avatar/14.jpg', 9);
 
 INSERT INTO SANPHAM(ID, TenSanPham, Gia, GiaMuaNgay, ThoiGianConLai, NgayDang, MoTaNgan, MoTaDai, MainImg, SoLanDuocDauGia, IDLoai, IDNguoiBan, IDNguoiThangDauGia) VALUES
-(null, 'sp', 100000, 1000000, '00:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/1/main.jpg', 10, 1, 2, null),
-(null, 'sp', 700000, 2000000, '01:01:01', '2019-11-30 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/2/main.jpg', 10, 2, 12, null),
-(null, 'sp', 250000, 1000000, '01:01:01', '2019-11-27 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/3/main.jpg', 100, 3, 4, null),
-(null, 'sp', 360000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/4/main.jpg', 10, 1, 4, null),
-(null, 'sp', 800000, 1000000, '07:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/5/main.jpg', 10, 2, 10, null),
-(null, 'sp', 400000, 4000000, '01:01:01', '2019-11-25 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/6/main.jpg', 10, 3, 6, null),
-(null, 'sp', 410000, 1400000, '00:12:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/7/main.jpg', 200, 1, 8, null),
-(null, 'sp', 500000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/8/main.jpg', 10, 2, 8, null),
-(null, 'sp', 300000, 1000000, '00:03:59', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/9/main.jpg', 10, 3, 14, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/10/main.jpg', 10, 1, 6, null),
-(null, 'sp', 100000, 3000000, '13:01:01', '2019-11-12 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/11/main.jpg', 10, 2, 4, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-2 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/12/main.jpg', 300, 3, 4, null),
-(null, 'sp', 350000, 1000000, '02:01:01', '2019-11-11 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/13/main.jpg', 10, 1, 4, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/14/main.jpg', 10, 2, 2, null),
-(null, 'sp', 100000, 5000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/15/main.jpg', 10, 3, 12, null),
-(null, 'sp', 400000, 2000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/16/main.jpg', 10, 1, 2, null),
-(null, 'sp', 510000, 1000000, '00:40:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/17/main.jpg', 400, 2, 8, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/18/main.jpg', 10, 3, 6, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-17 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/19/main.jpg', 10, 1, 4, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/20/main.jpg', 10, 2, 2, null),
-(null, 'sp', 190000, 1000000, '00:23:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/21/main.jpg', 10, 3, 10, null),
-(null, 'sp', 100000, 1000000, '08:01:01', '2019-11-2 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/22/main.jpg', 500, 1, 14, null),
-(null, 'sp', 100000, 1000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/23/main.jpg', 10, 2, 6, null);
-				
+(null, 'sp', 100000, 2000000, '00:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/1/main.jpg', 10, 1, 2, null),
+(null, 'sp', 700000, 2000000, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/2/main.jpg', 10, 2, 12, null),
+(null, 'sp', 250000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/3/main.jpg', 100, 3, 4, null),
+(null, 'sp', 360000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/4/main.jpg', 10, 4, 4, null),
+(null, 'sp', 800000, null, '07:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/5/main.jpg', 10, 5, 10, null),
+(null, 'sp', 400000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/6/main.jpg', 10, 6, 6, null),
+(null, 'sp', 410000, null, '00:12:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/7/main.jpg', 200, 7, 8, null),
+(null, 'sp', 500000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/8/main.jpg', 10, 8, 8, null),
+(null, 'sp', 300000, null, '00:03:59', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/9/main.jpg', 10, 9, 14, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/10/main.jpg', 10, 10, 6, null),
+(null, 'sp', 100000, null, '13:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/11/main.jpg', 10, 1, 4, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/12/main.jpg', 300, 1, 4, null),
+(null, 'sp', 350000, null, '02:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/13/main.jpg', 10, 2, 4, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/14/main.jpg', 10, 3, 2, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/15/main.jpg', 10, 4, 12, null),
+(null, 'sp', 400000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/16/main.jpg', 10, 3, 2, null),
+(null, 'sp', 510000, null, '00:40:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/17/main.jpg', 400, 2, 8, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/18/main.jpg', 10, 6, 6, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/19/main.jpg', 10, 7, 4, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/20/main.jpg', 10, 9, 2, null),
+(null, 'sp', 190000, null, '00:23:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/21/main.jpg', 10, 10, 10, null),
+(null, 'sp', 100000, null, '08:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/22/main.jpg', 500, 8, 14, null),
+(null, 'sp', 100000, null, '01:01:01', '2019-11-29 20:20:20', 'Mô tả ngắn', 'Mô tả dàiiiiiiiiiiiiiiiiiiiiiiiii', 'assets/images/product/23/main.jpg', 10, 1, 6, null);
+
 -- Folder product gồm các folder là ID của product, trong đó chứa 3-4 tấm hình của product đó
 -- Tên đường dẫn: assets/images/product/{{IDSanPham}}/{{IDHinh}}.jpg
 INSERT INTO HINHANH(IDSanPham, IDHinh, ImgURL) VALUES
 (1, 1, 'assets/images/product/1/1.jpg'),
 (1, 2, 'assets/images/product/1/2.jpg'),
 (1, 3, 'assets/images/product/1/3.jpg'),
-(1, 4, 'assets/images/product/1/4.jpg'),
 (2, 1, 'assets/images/product/2/1.jpg'),
 (2, 2, 'assets/images/product/2/2.jpg'),
 (2, 3, 'assets/images/product/2/3.jpg'),
@@ -238,6 +266,22 @@ INSERT INTO SANPHAMYEUTHICH(IDSanPham, IDNguoiDung) VALUES
 (7, 4),
 (7, 3);
 
+INSERT INTO DANHSACHCAM(IDNguoiDung, IDSanPham) VALUES
+(1, 3),
+(2, 2),
+(3, 20),
+(4, 8),
+(5, 19),
+(6, 5),
+(7, 12),
+(8, 15),
+(9, 1),
+(10, 14),
+(11, 13),
+(12, 11),
+(13, 17),
+(14, 6);
+
 -- YYYY-MM-DD hh:mm:ss
 INSERT INTO CHITIETDAUGIA(IDNguoiDauGia, IDSanPham, ThoiGianDauGia, Gia) VALUES
 (1, 9, '2019-12-06 15:23:23', 100000),
@@ -258,7 +302,7 @@ INSERT INTO CHITIETDAUGIA(IDNguoiDauGia, IDSanPham, ThoiGianDauGia, Gia) VALUES
 (7, 8, '2019-12-06 15:23:23', 500000),
 (8, 9, '2019-12-06 15:23:23', 200000),
 (7, 2, '2019-12-06 15:23:23', 700000),
-(1, 9, '2019-12-06 15:25:23', 300000),
+(12, 9, '2019-12-06 15:25:23', 300000),
 (10, 10, '2019-12-09 22:21:33', 100000),
 (14, 11, '2019-12-09 22:21:33', 100000),
 (3, 17, '2019-12-09 22:21:33', 500000),
@@ -327,19 +371,3 @@ SELECT SP1.*
 FROM SANPHAM SP1 JOIN SANPHAM SP2 ON SP1.IDLoai = SP2.IDLoai
 WHERE SP1.ID <> SP2.ID AND SP2.ID = 1
 ORDER BY RAND() LIMIT 5;
-
--- Hiển thị sản phẩm trong 1 loại và các thông tin ngắn gọn
-SELECT LOAI.TenLoai, SP.TenSanPham, SP.Gia, SP.GiaMuaNgay, SP.ThoiGianConLai, SP.NgayDang, SP.SoLanDuocDauGia, ND.TenTaiKhoan, SP.MainImg
-FROM LOAI LEFT JOIN SANPHAM SP ON LOAI.ID = SP.IDLoai
-	      LEFT JOIN NGUOIDUNG ND ON SP.IDNguoiBan = ND.ID
-WHERE LOAI.ID = 1
-ORDER BY SP.NgayDang DESC;
-
-SELECT *
-FROM NGUOIDUNG;
-
-SELECT SP.ID, SP.TenSanPham, SP.Gia, SP.GiaMuaNgay, SP.ThoiGianConLai, SP.NgayDang, SP.SoLanDuocDauGia, ND.TenTaiKhoan, SP.MainImg
-                FROM SANPHAMYEUTHICH YT LEFT JOIN SANPHAM SP ON YT.IDSanPham = SP.ID
-                LEFT JOIN NGUOIDUNG ND ON SP.IDNguoiBan = ND.ID
-                WHERE YT.IDNguoiDung = 1
-                ORDER BY SP.NgayDang DESC
