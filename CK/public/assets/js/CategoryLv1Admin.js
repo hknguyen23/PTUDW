e260cd59989f354031c5b49012f058f6fd0619e7
hknguyen23 <55338461+hknguyen23@users.noreply.github.com>
2020-01-06 09:37:13 +0700

    Add files via upload

--- CK/public/assets/js/CategoryLv1Admin.js
+++ CK/public/assets/js/CategoryLv1Admin.js
@@ -10,6 +10,7 @@ function editButtonClick(input) {
 	mainRow.insertCell(4).innerHTML = `<th style="text-align: right; width: 5%;" scope="col"></th>`;
 	$("#rowSelected").val(row.rowIndex);
 	$('button[id^="editButton"]').prop('disabled', true);			// disabled all button start with id="editButton"
+	$('button[id^="addButton"]').prop('disabled', true);
 	$(row).find("td").find(".catName").attr("readonly", false);
 }
 
@@ -17,6 +18,7 @@ function addButtonClick() {
 	var table = document.getElementById("myTable");
 	var row = table.insertRow(-1);
 	$('button[id^="addButton"]').prop('disabled', true);
+	$('button[id^="editButton"]').prop('disabled', true);
 	
 	var cell0 = row.insertCell(0);
 	cell0.innerHTML = `<th style="width: 5%;" scope="row"></th>`;
@@ -24,4 +26,20 @@ function addButtonClick() {
 	cell1.innerHTML = `<td><input name="newCatName" style="border: none; width: 100%;"></td>`;
 	var cell2 = row.insertCell(2);
 	cell2.innerHTML = `<td><button class="btn btn-success" type="submit" formaction="add"><i class="fa fa-plus fa-sm" aria-hidden="true"></i></button></td>`;
+}
+
+function checkQuantity() {
+	var table = document.getElementById("myTable");
+	var row = table.rows[$("#rowSelected").val()];
+	var quantity = row.cells[2].innerHTML;
+	var check = true;
+	if (quantity > 0){
+		if (confirm(`Danh mục này hiện có ${quantity} sản phẩm, bạn có chắc muốn xóa danh mục này không?`)){
+			check = true;
+		}
+		else {
+			check = false;
+		}
+	}
+	return check;
 }
--- CK/public/assets/js/CategoryLv2Admin.js
+++ CK/public/assets/js/CategoryLv2Admin.js
@@ -13,6 +13,7 @@ function editButtonClick(input) {
 	//$("#catName").prop('readonly', false);
 	//document.getElementById("catName").readOnly = false;
 	$('button[id^="editButton"]').prop('disabled', true);			// disabled all button start with id="editButton"
+	$('button[id^="addButton"]').prop('disabled', true);
 	$(row).find("td").find(".catName").attr("readonly", false);
 }
 
@@ -21,6 +22,7 @@ function addButtonClick() {
 	var row = table.insertRow(-1);
 	var parts = window.location.pathname.split('/');
 	$('button[id^="addButton"]').prop('disabled', true);
+	$('button[id^="editButton"]').prop('disabled', true);
 	
 	var cell0 = row.insertCell(0);
 	cell0.innerHTML = `<th style="width: 5%;" scope="row"><input name="idLoaiCap1" id="idLoaiCap1" style="border: none; width: 100%;" hidden></th>`;
@@ -29,4 +31,20 @@ function addButtonClick() {
 	cell1.innerHTML = `<td><input name="newCatName" style="border: none; width: 100%;"></td>`;
 	var cell2 = row.insertCell(2);
 	cell2.innerHTML = `<td><button class="btn btn-success" type="submit" formaction="add"><i class="fa fa-plus fa-sm" aria-hidden="true"></i></button></td>`;
+}
+
+function checkQuantity() {
+	var table = document.getElementById("myTable");
+	var row = table.rows[$("#rowSelected").val()];
+	var quantity = row.cells[2].innerHTML;
+	var check = true;
+	if (quantity > 0){
+		if (confirm(`Danh mục này hiện có ${quantity} sản phẩm, bạn có chắc muốn xóa danh mục này không?`)){
+			check = true;
+		}
+		else {
+			check = false;
+		}
+	}
+	return check;
 }
